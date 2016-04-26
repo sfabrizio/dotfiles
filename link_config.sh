f4729a57a77d22569bf0b72a4e75907cf6ad4800
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-04-26 15:50:49 -0300

    [ADD] vim monokai theme

--- link_config.sh
+++ link_config.sh
@@ -8,7 +8,8 @@ echo "Coping new files.."
 echo "[include] \npath = ~/dotfiles/gitconfig" > ~/.gitconfig
 echo "source ~/dotfiles/vimrc" > ~/.vimrc
 echo "source ~/dotfiles/zshrc" > ~/.zshrc
-
+mkdir -p ~/.vim/colors
+cp ~/dotfiles/vim/monokai.vim ~/.vim/colors/  
 source ~/.zshrc
 
 echo "Everything Done."

5a340dba15468cc124b553f4f6bce8e15682dc8e
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-04-26 13:30:46 -0300

    [ADD] improve link_config

--- link_config.sh
+++ link_config.sh
@@ -1,5 +1,14 @@
 
-echo "Creating backup of your gitconfig.."
-mv ~/.gitconfig ~/.gitconfig.bak
-echo "Done."
+echo "Creating backup of your previus config files."
+cp ~/.gitconfig ~/.gitconfig.bak
+cp ~/.vimrc ~/.vimrc.bak
+cp ~/.zshrc ~/.zshrc.bak
+
+echo "Coping new files.."
 echo "[include] \npath = ~/dotfiles/gitconfig" > ~/.gitconfig
+echo "source ~/dotfiles/vimrc" > ~/.vimrc
+echo "source ~/dotfiles/zshrc" > ~/.zshrc
+
+source ~/.zshrc
+
+echo "Everything Done."

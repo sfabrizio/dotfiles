7f126e138cdfd9849cd0ca5c4f0ae85e46093664
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-10-27 12:12:21 -0300

    [ADD] link_config script: add tmux.conf file

--- link_config.sh
+++ link_config.sh
@@ -1,13 +1,15 @@
-
 echo "Creating backup of your previus config files."
 cp ~/.gitconfig ~/.gitconfig.bak
 cp ~/.vimrc ~/.vimrc.bak
 cp ~/.zshrc ~/.zshrc.bak
+cp ~/.tmux.conf ~/.tmux.conf.bak
 
-echo "Coping new files.."
+echo "Coping new configuration files.."
 echo "[include] \npath = ~/dotfiles/gitconfig" > ~/.gitconfig
 echo "source ~/dotfiles/vimrc" > ~/.vimrc
 echo "source ~/dotfiles/zshrc" > ~/.zshrc
+echo "source ~/dotfiles/tmux.conf" > ~/.tmux.conf
+
 source ~/.zshrc
 
 echo "Everything Done."

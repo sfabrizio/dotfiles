20e416cff48b83522ee7e6cbfffab45ac1aa642c
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-11-18 00:38:40 -0300

    [ADD] sam-theme: improves on prompt
         - fix git erro & autocomplete aditional tab

--- sam-mac-shell.sh
+++ sam-mac-shell.sh
@@ -1,4 +1,3 @@
-local nvm_status="%{$fg_bold[yellow]%}⬢ "
 local ret_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})"
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
@@ -7,23 +6,30 @@ ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
 
 SAM_THEME_NVM_SIMBOL="%{$fg_bold[green]%}⬢"
-SAM_THEME_NVM_PREFIX="%{$fg_bold[yellow]%}("
-SAM_THEME_NVM_SUFFIX="%{$fg_bold[yellow]%})"
+SAM_THEME_NVM_PREFIX="%{$fg_bold[green]%}(%{$reset_color%}"
+SAM_THEME_NVM_SUFFIX="%{$fg_bold[green]%})%{$reset_color%}"
+SAM_THEME_ICON="🔥"
+SAM_THEME_ICON2="💥"
 
 function nvm_prompt_info {
-    if [[ $(command git branch > /dev/null) ]]; then
-        return ""
+    if command git >/dev/null 2>/dev/null; then
+        return
     fi
-    local mainPathPackageJson=$(git rev-parse --show-toplevel)"/package.json"
+    local mainPathPackageJson=$(git rev-parse --show-toplevel 2>/dev/null)"/package.json"
     if [[ -f $mainPathPackageJson ]]; then
-        local result
+        local result=""
         result+=$SAM_THEME_NVM_SIMBOL" "
         result+=$SAM_THEME_NVM_PREFIX
-        result+=$(nvm current)
+        result+=%{$fg[yellow]%}$(nvm current)
         result+=$SAM_THEME_NVM_SUFFIX
         echo "$result"
     fi
 }
 
 
-PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)$(nvm_prompt_info)🔥 $reset_color '
+PROMPT=''
+PROMPT+='${ret_status}'
+PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%}'
+PROMPT+='$(git_prompt_info)'
+PROMPT+='$(nvm_prompt_info)'
+PROMPT+=' $SAM_THEME_ICON '

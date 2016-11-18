9a58ea7072c607f351b84bd7390bd8e921a88f14
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-11-17 22:48:59 -0300

    [MOD] move sam-theme to sh file

---
+++ sam-mac-shell.sh
@@ -0,0 +1,29 @@
+local nvm_status="%{$fg_bold[yellow]%}⬢ "
+local ret_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})"
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+
+SAM_THEME_NVM_SIMBOL="%{$fg_bold[green]%}⬢"
+SAM_THEME_NVM_PREFIX="%{$fg_bold[yellow]%}("
+SAM_THEME_NVM_SUFFIX="%{$fg_bold[yellow]%})"
+
+function nvm_prompt_info {
+    if [[ $(command git branch > /dev/null) ]]; then
+        return ""
+    fi
+    local mainPathPackageJson=$(git rev-parse --show-toplevel)"/package.json"
+    if [[ -f $mainPathPackageJson ]]; then
+        local result
+        result+=$SAM_THEME_NVM_SIMBOL" "
+        result+=$SAM_THEME_NVM_PREFIX
+        result+=$(nvm current)
+        result+=$SAM_THEME_NVM_SUFFIX
+        echo "$result"
+    fi
+}
+
+
+PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)$(nvm_prompt_info)🔥 $reset_color '

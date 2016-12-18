309584620ee89d7ba4ee3f8d9a187ad21f6d1824
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-12-18 19:15:24 -0300

    [MOD] sam-theme: change `git:` to an icon

--- sam-mac-shell.sh
+++ sam-mac-shell.sh
@@ -1,7 +1,7 @@
 source ~/dotfiles/scripts/os_detection.sh
 
 #git values
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}\ue709 (%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"

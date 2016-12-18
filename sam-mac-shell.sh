55f2ba5ff837493955dd391d5f8d3793e6acf5dd
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-12-18 19:41:42 -0300

    [ADD] sam-theme: prompt git status with icons

--- sam-mac-shell.sh
+++ sam-mac-shell.sh
@@ -49,6 +49,45 @@ function print_nvm_info {
     fi
 }
 
+# Git: branch/detached head, dirty status
+prompt_git_status() {
+  (( $+commands[git] )) || return
+  local ref dirty mode repo_path
+  repo_path=$(git rev-parse --git-dir 2>/dev/null)
+
+  if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
+    dirty=$(parse_git_dirty)
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="➦ $(git rev-parse --short HEAD 2> /dev/null)"
+    if [[ -n $dirty ]]; then
+      echo -n "%{$fg_bold[red]%}"
+    else
+      echo -n "%{$fg_bold[cyan]%}"
+    fi
+
+    if [[ -e "${repo_path}/BISECT_LOG" ]]; then
+      mode=" <B>"
+    elif [[ -e "${repo_path}/MERGE_HEAD" ]]; then
+      mode=" >M<"
+    elif [[ -e "${repo_path}/rebase" || -e "${repo_path}/rebase-apply" || -e "${repo_path}/rebase-merge" || -e "${repo_path}/../.dotest" ]]; then
+      mode=" >R>"
+    fi
+
+    setopt promptsubst
+    autoload -Uz vcs_info
+
+    zstyle ':vcs_info:*' enable git
+    zstyle ':vcs_info:*' get-revision true
+    zstyle ':vcs_info:*' check-for-changes true
+    zstyle ':vcs_info:*' stagedstr "%{$FG[154]%}"'\uf103 '
+    zstyle ':vcs_info:*' unstagedstr "%{$FG[197]%}"'\uf0c3 '
+    zstyle ':vcs_info:*' formats ' %u%c'
+    zstyle ':vcs_info:*' actionformats ' %u%c'
+    vcs_info
+    echo -n "${vcs_info_msg_0_%%}${mode}"
+  fi
+  echo -n "%{$reset_color%}"
+}
+
 #Final prompt concat
 PROMPT=''
 PROMPT+='${ret_status}'

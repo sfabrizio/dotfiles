b7fae95bca9004673cd9934719ac8e2a2cafb87f
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-12-23 00:41:45 -0300

    [MOD] script: re-commit parameters order
         - [FIX] missing semicolon

--- scripts/re-commit.sh
+++ scripts/re-commit.sh
@@ -1,5 +1,5 @@
-export datesam=$1 && export commitsam=$2 && git filter-branch -f --env-filter \
-    'if [ $GIT_COMMIT = $commitsam ] then
+export datesam=$2 && export commitsam=$1 && git filter-branch -f --env-filter \
+    'if [ $GIT_COMMIT == $commitsam ] ; then
 	     export GIT_AUTHOR_DATE="$datesam"
          export GIT_COMMITER_DATE="$datesam"
      fi'

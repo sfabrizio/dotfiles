d4e87214624576e27be8ab96d2fb1a3695c26f4a
Samuel Fabrizio <samuelfabrizio@gmail.com>
2017-05-09 00:23:42 +0200

    [ADD] install: now install autoenv also

--- install.sh
+++ install.sh
@@ -7,13 +7,16 @@ cp ~/.vimrc ~/.vimrc.bak
 cp ~/.zshrc ~/.zshrc.bak
 cp ~/.tmux.conf ~/.tmux.conf.bak
 
-echo "creating workspace folder."
+echo "creating folders"
 cd ~/
 mkdir -p workspace
+mkdir -p .tmux
+mkdir -p .autoenv
 
 echo "cloning git repos on workspace folder"
-cd ~/workspace
-git clone https://github.com/sfabrizio/ozono-zsh-theme
+git clone https://github.com/sfabrizio/ozono-zsh-theme ~/workspace
+git clone https://github.com/erikw/tmux-powerline.git ~/.tmux
+git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 
 echo "Coping new configuration files.."
 echo "[include] path = ~/dotfiles/gitconfig" > ~/.gitconfig

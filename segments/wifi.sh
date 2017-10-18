8c965294dfc0309f73a82713bd9b0a0871a9d26a
Samuel Fabrizio <samuelfabrizio@gmail.com>
2017-10-18 19:20:10 +0200

    [ADD] segment wifi: add no conection case

--- segments/wifi.sh
+++ segments/wifi.sh
@@ -2,6 +2,10 @@
 
 run_segment() {
     echo -n " "
-    echo -e "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')"
+    echo -e "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I \
+            | awk '/ SSID/ {print substr($0, index($0, $2))}'\
+            | sed 's/SSID:/No Connection/g'\
+            | tr -d '[:blank:]')"
 	exit 0
 }
+

72867d50ad8fd8279d33326e2731a8ad73e6621c
Samuel Fabrizio <samuelfabrizio@gmail.com>
2017-10-19 21:26:23 +0200

    [ADD] segment wifi: show wifi off case
         - code improvements also

--- segments/wifi.sh
+++ segments/wifi.sh
@@ -1,11 +1,22 @@
 # Print wifi info.
 
+osxCommand='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I'
+icon=" "
+
+
 run_segment() {
-    echo -n " "
-    echo -e "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I \
-            | awk '/ SSID/ {print substr($0, index($0, $2))}'\
-            | sed 's/SSID:/No Connection/g'\
-            | tr -d '[:blank:]')"
+
+    if echo -n `$osxCommand` | grep -q "AirPort: Off"; then
+        echo -e $icon "Off"
+        exit 0;
+    fi
+
+    echo -n "$icon"
+    echo -e "$( $osxCommand \
+             | awk '/ SSID/ {print substr($0, index($0, $2))}'\
+             | sed 's/SSID:/No_connection/g'\
+             | tr -d '[:blank:]' \
+            )"
 	exit 0
 }
 

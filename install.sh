650c07235493169560e4db40b3727143dffa9016
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-05-26 20:55:23 +0200

    [ADD] install: set zsh as default terminal

--- install.sh
+++ install.sh
@@ -49,6 +49,8 @@ fi
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+chsh -s `which zsh`
+
 if ! [ -d ~/.nvm ]; then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 fi

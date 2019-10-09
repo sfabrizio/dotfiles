11657fcaf879d0fb377a3da4ffde79aa6ac96498
Samuel Fabrizio <samuelfabrizio@gmail.com>
2019-10-09 19:32:00 +0200

    [FIX] segment: macos_notification_count2
         - fix for mojave db location

--- segments/macos_notification_count2.sh
+++ segments/macos_notification_count2.sh
@@ -32,13 +32,13 @@ run_segment() {
 	__process_settings
 
 	local db_location app_ids_array query_condition query_string count
-	db_location="$(getconf DARWIN_USER_DIR)/com.apple.notificationcenter/db/db"
+	db_location="$(getconf DARWIN_USER_DIR)/com.apple.notificationcenter/db2/db"
 	app_ids_array=(${TMUX_POWERLINE_SEG_MACOS_NOTIFICATION_COUNT_APPIDS2})
 	query_condition="where app_id=${app_ids_array[0]}"
 	for app_id in "${app_ids_array[@]:1}"; do
 		query_condition="${query_condition} OR app_id=${app_id}"
 	done
-    query_string="select count(*) from notifications ${query_condition}"
+    query_string="select count(*) from record ${query_condition}"
 	count=$(sqlite3 -list ${db_location} "${query_string}")
 
 	local exitcode="$?"

962dea3c6a6aab2714db4519e0cd27ca25231d10
Samuel Fabrizio <samuelfabrizio@gmail.com>
2024-09-16 17:25:02 +0200

    [ADD] script for download patch font
         - no more manually doing it

---
+++ scripts/nerd-font-download.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+mkdir -p ~/.fonts
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+echo "done!"
